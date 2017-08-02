cd5fa4cdb120b80c1b8ab70ada87b293323993cd
Mike Fechner <mike.fechner@consultingwerk.de>
2017-08-02 15:43:25 +0200

    Use a timer to refresh list of agents after trim

--- src/app/appserver-manager/appserver-manager.component.ts
+++ src/app/appserver-manager/appserver-manager.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SmartHttpService, SmartServiceAdapter } from "@consultingwerk/smartcomponents-core";
 import { Headers, RequestOptions } from '@angular/http';
+import { Observable } from "rxjs";
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'appserver-manager',
@@ -19,69 +21,75 @@ export class AppserverManagerComponent implements OnInit {
   protected fetchApplicationResult: any;
 
   constructor(public serviceAdapter: SmartServiceAdapter,
-              private smartHttp: SmartHttpService) {
+    private smartHttp: SmartHttpService) {
 
-   }
+  }
 
   ngOnInit() {
   }
 
   fetchApplications() {
 
-      let headers: Headers = new Headers();
-      headers.append("Authorization", "Basic " + btoa(this.userName + ":" + this.password));
-      let options = new RequestOptions({ headers: headers, withCredentials: true });
+    let headers: Headers = new Headers();
+    headers.append("Authorization", "Basic " + btoa(this.userName + ":" + this.password));
+    let options = new RequestOptions({ headers: headers, withCredentials: true });
 
-      this.smartHttp.get(`${this.serviceAdapter.serviceURI}/oemanager/applications`, options)
-            .subscribe(response => {
-              const json = response.json();
+    this.smartHttp.get(`${this.serviceAdapter.serviceURI}/oemanager/applications`, options)
+      .subscribe(response => {
+        const json = response.json();
 
-              this.fetchApplicationResult = json;
+        this.fetchApplicationResult = json;
 
-              this.applications = [];
-              console.log (json);
+        this.applications = [];
+        console.log(json);
 
-              json.result.Application.forEach(element => {
-                this.applications.push (element.name);
-              });
+        json.result.Application.forEach(element => {
+          this.applications.push(element.name);
+        });
 
-              if (this.applications.length > 0) {
-                this.selectedApplication = this.applications [0];
-                this.fetchServers();
-              } else {
-                this.selectedApplication = '';
-              }
-            });
+        if (this.applications.length > 0) {
+          this.selectedApplication = this.applications[0];
+          this.fetchServers();
+        } else {
+          this.selectedApplication = '';
+        }
+      });
   }
 
-  fetchServers() {
+  fetchServers(): Observable<Response> {
 
-      let headers: Headers = new Headers();
-      headers.append("Authorization", "Basic " + btoa(this.userName + ":" + this.password));
-      let options = new RequestOptions({ headers: headers, withCredentials: true });
+    let headers: Headers = new Headers();
+    headers.append("Authorization", "Basic " + btoa(this.userName + ":" + this.password));
+    let options = new RequestOptions({ headers: headers, withCredentials: true });
 
-      this.smartHttp.get(`${this.serviceAdapter.serviceURI}/oemanager/applications/${this.selectedApplication}/agents`, options)
-            .subscribe(response => {
-              const json = response.json();
-              this.servers = json.result.agents;
-            });
+    let observable = this.smartHttp.get(`${this.serviceAdapter.serviceURI}/oemanager/applications/${this.selectedApplication}/agents`, options);
+    observable.subscribe(response => {
+      const json = response.json();
+      this.servers = json.result.agents;
+    });
+    return observable;
   }
 
   trimServers() {
 
-      let headers: Headers = new Headers();
-      headers.append("Authorization", "Basic " + btoa(this.userName + ":" + this.password));
-      let options = new RequestOptions({ headers: headers, withCredentials: true });
+    let headers: Headers = new Headers();
+    headers.append("Authorization", "Basic " + btoa(this.userName + ":" + this.password));
+    let options = new RequestOptions({ headers: headers, withCredentials: true });
 
-      this.servers.forEach(element => {
+    this.servers.forEach(element => {
 
       this.smartHttp.delete
         (`${this.serviceAdapter.serviceURI}/oemanager/applications/${this.selectedApplication}/agents/${element.agentId}`, options)
-            .subscribe(response => {
-              console.log (response);
-            });
-      });
+        .subscribe(response => {
+          console.log(response);
+        });
+    });
 
-      this.fetchServers();
+    this.fetchServers().subscribe(() => {
+      // pause 1 second and then call fetchServers() again
+      setTimeout(() => {
+        this.fetchServers();
+      }, 2000);
+    });
   }
 }
