13b18a3e72db2070c1c5cbe7dd5f6791a009d9c5
Radu Nicoara <radu.nicoara@acorn.ro>
2017-10-12 13:37:45 +0300

    set locale de-DE for kendo widgets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { PanelBarModule } from '@progress/kendo-angular-layout';
 import { SessionInfoController } from './sessioninfo.controller';
 import { StartPageController } from './start-page.controller';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -20,7 +20,27 @@ import { FileInfoModule } from './file-info/file-info.module';
 import { AppserverManagerModule } from './appserver-manager/appserver-manager.module';
 import { LogoutComponent } from './logout/logout.component';
 
+import { load } from '@progress/kendo-angular-intl';
+import * as likelySubtags from 'cldr-data/supplemental/likelySubtags.json';
+import * as weekData from 'cldr-data/supplemental/weekData.json';
+import * as currencyData from 'cldr-data/supplemental/currencyData.json';
+import * as numbers from 'cldr-data/main/de/numbers.json';
+import * as timeZoneNames from 'cldr-data/main/de/timeZoneNames.json';
+import * as calendar from 'cldr-data/main/de/ca-gregorian.json';
+import * as currencies from 'cldr-data/main/de/currencies.json';
+import * as dateFields from 'cldr-data/main/de/dateFields.json';
 import { CustomerFormController } from './controllers/customer-form.controller';
+
+load(
+  likelySubtags,
+  weekData,
+  currencyData,
+  numbers,
+  currencies,
+  calendar,
+  dateFields,
+  timeZoneNames
+);
 
 @NgModule({
   declarations: [
@@ -66,7 +86,9 @@ import { CustomerFormController } from './controllers/customer-form.controller';
     }],
     { useHash: true })
   ],
-  providers: [],
+  providers: [
+    {  provide: LOCALE_ID, useValue: 'de-DE' }
+  ],
   bootstrap: [CustomRootComponent]
 })
 export class AppModule { }
