9242f8e6dfcdb48450eac8592c4504dca9074148
Radu Nicoara <nicoara.radu16@gmail.com>
2018-10-02 14:07:38 +0300

    annotations based customer form

---
+++ src/app/forms/customer-maintenance-annotations/customer-maintenance-annotations.form.ts
@@ -0,0 +1,146 @@
+import { SmartComponentLibraryModule, SmartFormComponent, CustomSmartForm, DataSourceRegistry, SmartViewerRegistryService, SmartTabFolderRegistryService, SmartViewManagerService, SmartFormInstanceService, SmartToolbarRegistry, SmartDataSource, SmartComboEditorComponent, WidgetFacadeFactory, SmartDialogService, SmartNavigationService, SmartToolbarComponent, DialogButtons } from '@consultingwerk/smartcomponent-library';
+import { Component, Injector, OnInit, OnDestroy, OnChanges, SimpleChanges, NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common';
+import { first, filter, map } from 'rxjs/operators';
+
+
+@CustomSmartForm('customerFormAnnotationBased')
+@Component({
+    selector: 'customer-maintenance-annotations-form',
+    templateUrl: '../../../../node_modules/@consultingwerk/smartcomponent-library/ui/form/smart-form.component.html',
+    viewProviders: [DataSourceRegistry, SmartViewManagerService, SmartFormInstanceService, SmartToolbarRegistry, SmartViewerRegistryService, SmartTabFolderRegistryService]
+})
+export class CustomerMaintenanceAnnotationsFormComponent extends SmartFormComponent implements OnInit, OnDestroy, OnChanges {
+
+    protected customerDatasource: SmartDataSource;
+    protected customerCountryCombo: SmartComboEditorComponent;
+    
+    constructor(injector: Injector,
+        private toolbarRegistry: SmartToolbarRegistry,
+        private viewerRegistry: SmartViewerRegistryService,
+        private dsRegistry: DataSourceRegistry,
+        private widgetFactory: WidgetFacadeFactory,
+        private dialogService: SmartDialogService,
+        private navigationService: SmartNavigationService) {
+        super(injector);
+    }
+
+    async ngOnInit() {
+        // Add your own initialization logic here
+        
+        this.setFormConfiguration('/SmartForm/Form/Consultingwerk.SmartComponentsDemo.OERA.Sports2000.CustomerBusinessEntity/customer');
+        
+        super.ngOnInit();
+
+        this.viewerRegistry.smartViewerAdded.pipe(first(viewer => viewer.name === 'CustomerViewer'))
+            .subscribe(customerViewer => {
+                customerViewer.inputValueChanged.subscribe(() => {
+                    this.setStateInputSensitivity();
+                });
+            });
+
+        this.toolbarRegistry.toolbarRegistered.pipe(
+            filter(ev => ev.toolbarName === 'DefaultToolbar'),
+            map(ev => ev.toolbar)
+        ).subscribe((toolbar: SmartToolbarComponent) => {
+            toolbar.buttonClicked.subscribe(buttonName => this.handleToolbarButtonClick(buttonName));
+            this.customerCountryCombo = toolbar.getComboControl('country-combo');
+            this.customerCountryCombo.smartModelChanged.subscribe(value => {
+                this.customerDatasource.selected.Country = value;
+                this.customerDatasource.updatingRecord = true;
+            });
+
+        });
+
+        console.log('got viewer');
+        const customerDataSource = this.dsRegistry.getDataSource('customerDataSource') || await this.dsRegistry.dataSourceAdded.pipe(
+            first(ev => ev.dataSourceName === 'customerDataSource'),
+            map(ev => ev.dataSource)
+        ).toPromise();
+        console.log('got data source');
+        this.customerDatasource = customerDataSource;
+        console.log(this.customerDatasource)
+        customerDataSource.selectionChanged.subscribe(selectionEvent => {
+            this.setStateInputSensitivity();
+        });
+        customerDataSource.stateChanged.subscribe(() => {
+            this.setStateInputSensitivity();
+        });
+
+    }
+
+    private handleToolbarButtonClick(buttonName: string) {
+        console.log('button clicked: %s', buttonName);
+        switch (buttonName) {
+            case 'customer-orders':
+                this.navigationService.navigate('/customer-maint-anno/:CustNum/orders', {
+                    paramMap: {
+                        CustNum: this.customerDatasource.selected.CustNum
+                    }
+                });
+                break;
+            case 'customer-credithold':
+                this.PutCustomerOnHoldHandler(this.customerDatasource.selected);
+                break;
+        }
+    }
+
+    private setStateInputSensitivity() {
+        setTimeout(async () => {
+            const customerCountryInput = await this.widgetFactory.GetFacade('Country');
+            const customerStateInput = await this.widgetFactory.GetFacade('State');
+
+            if (customerCountryInput.SCREEN_VALUE) {
+                customerStateInput.SENSITIVE = customerCountryInput.SCREEN_VALUE.toUpperCase() === 'USA';
+            }
+            else {
+                customerStateInput.SENSITIVE = false;
+            }
+        });
+    }
+
+    async PutCustomerOnHoldHandler(customer: any) {
+        const result = await this.dialogService.showDialog({
+            buttons: [DialogButtons.YES, DialogButtons.CANCEL],
+            textContent: 'Are you sure you want to put this customer on hold?',
+            title: 'Confirm'
+        });
+        if (result.button.value.toLowerCase() === 'yes') {
+            await this.customerDatasource.invokeMethod('PutCustomerOnHold', {
+                plcParameter: {
+                    CustNum: this.customerDatasource.selected.CustNum,
+                    Comments: 'Welcome to TypeScript'
+                }
+            })
+            this.customerDatasource.fetch(<any>{
+                top: this.customerDatasource.top,
+                skip: this.customerDatasource.skipRecords
+            });
+        }
+    }
+
+    ngOnDestroy() {
+
+        super.ngOnDestroy();
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+
+        super.ngOnChanges(changes);
+    }
+
+}
+
+@NgModule({
+    imports: [
+        CommonModule,
+        SmartComponentLibraryModule
+    ],
+    declarations: [
+        CustomerMaintenanceAnnotationsFormComponent
+    ],
+    entryComponents: [
+        CustomerMaintenanceAnnotationsFormComponent
+    ]
+})
+export class CustomerMaintenanceAnnotationsFormModule { }
